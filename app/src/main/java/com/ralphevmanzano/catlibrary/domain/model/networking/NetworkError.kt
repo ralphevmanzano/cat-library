70159e0326b085a5728c3446d07313b7bf2f50f3
Ralph Manzano <ralphevmanzano@gmail.com>
2025-03-03 03:19:14 +0800

    Added projectstructure.png

--- app/src/main/java/com/ralphevmanzano/catlibrary/domain/model/networking/NetworkError.kt
+++ app/src/main/java/com/ralphevmanzano/catlibrary/domain/model/networking/NetworkError.kt
@@ -1,20 +1,10 @@
 package com.ralphevmanzano.catlibrary.domain.model.networking
 
-//enum class NetworkError: Error {
-//    NO_INTERNET_CONNECTION,
-//    TIMEOUT,
-//    TOO_MANY_REQUESTS,
-//    NOT_FOUND,
-//    UNKNOWN
-//}
-
 sealed interface NetworkError: Error {
     data object NoInternetConnection: NetworkError
     data object Timeout: NetworkError
     data object TooManyRequests: NetworkError
-    data class NotFound(
-        val message: String
-    ): NetworkError
+    data class NotFound(val message: String): NetworkError
     data object Unknown: NetworkError
     data object SerializationError: NetworkError
 }
